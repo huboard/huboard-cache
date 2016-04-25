71b524d74c7fede2ab1b0c0e842d2a8a97afb357
Dru Sellers <dru@drusellers.com>
2016-04-24 19:05:18 -0500

    Better run support

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
+if [ -e ".env" ]
+then
+    #do nothing
+else
+    #load default connection string
+    echo "PG=User ID=hucache;Password=;Host=localhost;Port=5432;Database=hucache;
+Pooling=false;" > .env
+fi
+
+source .env
 mono ./build/hucache.http.exe
