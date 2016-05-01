a11e2029c1bddf73d25143b333540faf520d28ea
Dru Sellers <dru@drusellers.com>
2016-05-01 07:59:49 -0500

    Adding docker support

--- hucache.http/Program.fs
+++ hucache.http/Program.fs
@@ -5,18 +5,29 @@ open Suave.Successful      // for OK-result
 open Suave.Web             // for config
 open Suave.Filters
 open Suave.Operators
+open System.Net
 open hucache.http.actions
 
 
 
-module Program = 
-  let app = 
-    choose 
+module Program =
+  let app =
+    choose
       [ GET >=> choose
           [ pathScan "/cache/%s/%s/issues/%d" getIssue
             pathScan "/cache/%s/%s/issues" getIssues ]]
 
+  let port =
+      let couldParse, parsedInt = System.UInt16.TryParse(System.Environment.GetEnvironmentVariable("PORT"))
+      if couldParse then parsedInt
+      else 5000us
+      
+  let config =
+    let ip = IPAddress.Parse "0.0.0.0"
+    {defaultConfig with
+      bindings = [HttpBinding.mk HTTP ip port ] }
+
   [<EntryPoint>]
-  let main argv =           
-    startWebServer defaultConfig app
+  let main argv =
+    startWebServer config app
     0
